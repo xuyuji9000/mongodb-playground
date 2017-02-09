393c30701ad4d2259e1cb0075a40a74a953ed482
Karl Xu <karl.yogi.xu@gmail.com>
2017-02-09 22:01:45 +0800

    add mongodb

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient; 
 
-const app = express();
-
-app.get('/', function(req, res){
-    res.sendFile(__dirname + '/index.html');
-});
-
-app.listen(3001, function() {
-    console.log('listen on 3001');
+MongoClient.connect('mongodb://admin:admin123@ds147789.mlab.com:47789/testmongo', (err,database) => {
+    const app = express();
+    app.use(bodyParser.urlencoded({extended:true}));
+    
+    app.get('/', function(req, res){
+        res.sendFile(__dirname + '/index.html');
+    });
+    
+    app.post('/quote', (req, res) => {
+        console.log(req.body);
+    });
+    
+    app.listen(3001, function() {
+        console.log('listen on 3001');
+    });
 });
