210105d23e861bee422b5f5fbca811a33c99ca43
Karl Xu <karl.yogi.xu@gmail.com>
2017-02-09 20:38:04 +0800

    express respond file

--- server.js
+++ server.js
@@ -3,9 +3,8 @@ const express = require('express');
 const app = express();
 
 app.get('/', function(req, res){
-    res.send('Hello World.');
+    res.sendFile(__dirname + '/index.html');
 });
-
 
 app.listen(3000, function() {
     console.log('listen on 3000');
