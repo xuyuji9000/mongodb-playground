8425ecfe2df21699c7b094d2472db444a0194ef5
Karl Xu <karl.yogi.xu@gmail.com>
2017-02-09 20:41:57 +0800

    add nodemon

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(3000, function() {
-    console.log('listen on 3000');
+app.listen(3001, function() {
+    console.log('listen on 3001');
 });
